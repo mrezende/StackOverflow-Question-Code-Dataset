8939f5dc1f7d9320fd0dc397cce7deafe6f50f02
Marcelo Martins <rezende.martins@gmail.com>
2019-07-23 21:07:05 -0300

    Update SqlTemplate.py

--- data_processing/codenn/src/sql/SqlTemplate.py
+++ data_processing/codenn/src/sql/SqlTemplate.py
@@ -114,7 +114,7 @@ class SqlTemplate:
     if isinstance(tok, sqlparse.sql.TokenList):
       tmpChildren = []
       for c in tok.tokens:
-        if not c.is_whitespace():
+        if not c.is_whitespace:
             tmpChildren.append(c)
 
       tok.tokens = tmpChildren
